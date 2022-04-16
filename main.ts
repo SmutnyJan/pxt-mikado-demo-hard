e4730552cd605e1f2ca0cc366c7e9965caf816f4
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-04-16 16:37:52 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,49 @@
-
+let jeZapnutoHlidani = false
+let akcelerace = 0
+let stavDisleje = ""
+input.onButtonPressed(Button.A, function () {
+    if (jeZapnutoHlidani == false) {
+        jeZapnutoHlidani = true
+    } else {
+        jeZapnutoHlidani = false
+    }
+})
+function detekujPohyb (tolerance: number) {
+    akcelerace = input.acceleration(Dimension.Strength)
+    if (akcelerace + tolerance < mikado.normalValue() || akcelerace - tolerance > mikado.normalValue()) {
+        return true
+    } else {
+        return false
+    }
+}
+basic.forever(function () {
+    if (detekujPohyb(100) == true && jeZapnutoHlidani && stavDisleje != "nastvany") {
+        stavDisleje = "nastvany"
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        soundExpression.sad.playUntilDone()
+    } else if (jeZapnutoHlidani && stavDisleje != "pozor") {
+        stavDisleje = "pozor"
+        basic.showLeds(`
+            # # . # #
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    } else if (!(jeZapnutoHlidani) && stavDisleje != "stastny") {
+        stavDisleje = "stastny"
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
+})
